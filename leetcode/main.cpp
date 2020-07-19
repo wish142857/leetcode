19a9fc976795376125e7b3c7420af11abf0c65cf
wish142857 <wish142857@163.com>
2020-07-19 15:50:18 +0800

    update #3: solve s478

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -1,7 +1,12 @@
+#include "solution.hpp"
 #include <iostream>
+
 using namespace std;
 
 int main() {
-	cout << "Hello" << endl;
+	s478::Solution solution(100.0, 200.0 , 200.0);
+
+
+	cout << solution.randPoint()[0] << solution.randPoint()[1] << endl;
 	return 0;
 }
--- leetcode/solution.hpp
+++ leetcode/solution.hpp
@@ -2,7 +2,8 @@
 #include <ctime>
 #include <iostream>
 #include <random>
-
+#include <vector>
+using std::vector;
 
 namespace s470 {
     //---------------------------------------------
@@ -32,4 +33,41 @@ namespace s470 {
             return (x % 10) + 1;
         }
     };
+}
+
+namespace s478 {
+    //---------------------------------------------
+    // @Date: 2020/7/19 
+    // @Algorithm: Rejection Sampling Algorithm
+    // @Time: O(1)
+    // @Space: O(1)
+    //---------------------------------------------
+    class Solution {
+    public:
+        std::mt19937 rng{ std::random_device{}() };
+        std::uniform_real_distribution<double> uni{ 0, 1 };
+
+        double r, x, y;
+        Solution(double radius, double x_center, double y_center) {
+            r = radius;
+            x = x_center;
+            y = y_center;
+        }
+
+        vector<double> randPoint() {
+            double rx, ry;
+            do {
+                rx = uni(rng) * 2 - 1;
+                ry = uni(rng) * 2 - 1;
+            } while (pow(rx, 2) + pow(ry, 2) > 1);
+            return vector<double>{ x + rx * r, y + ry * r };
+
+        }
+    };
+
+    /**
+     * Your Solution object will be instantiated and called as such:
+     * Solution* obj = new Solution(radius, x_center, y_center);
+     * vector<double> param_1 = obj->randPoint();
+     */
 }
