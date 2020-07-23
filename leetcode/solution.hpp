b5e126500ed52204cdc38c0a7858406d47236fa9
wish142857 <wish142857@163.com>
2020-07-23 23:52:53 +0800

    update #18: new solutions

--- leetcode/solution.hpp
+++ leetcode/solution.hpp
@@ -219,6 +219,34 @@ namespace s55 {
 
 
 //---------------------------------------------
+// @ID: #64
+// @Date: 2020/7/23
+// @Algorithm: Dynamic Algorithm
+// @Time: O(n*m)
+// @Space: O(n*m)
+//---------------------------------------------
+namespace s64 {
+    class Solution {
+    public:
+        int minPathSum(vector<vector<int>>& grid) {
+            if (grid.size() == 0 || grid[0].size() == 0)
+                return 0;
+            int m = grid.size(), n = grid[0].size();
+            vector<vector<int>> dp(m, vector<int>(n));
+            dp[0][0] = grid[0][0];
+            for (int j = 1; j < n; j++)
+                dp[0][j] = dp[0][j - 1] + grid[0][j];
+            for (int i = 1; i < m; i++) {
+                dp[i][0] = dp[i - 1][0] + grid[i][0];
+                for (int j = 1; j < n; j++)
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+            return dp[m - 1][n - 1];
+        }
+    };
+}
+
+//---------------------------------------------
 // @ID: #95
 // @Date: 2020/7/21
 // @Algorithm: Tree Algorithm
@@ -285,7 +313,7 @@ namespace s122 {
 
 
 //---------------------------------------------
-// @ID: #154
+// @ID: #153
 // @Date: 2020/7/22
 // @Algorithm: Binary Search Algorithm
 // @Time: O(log n)
