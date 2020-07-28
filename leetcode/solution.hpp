05ed6d98f8f84e9b6b9caf3404fcd40ebea4df3b
wish142857 <wish142857@163.com>
2020-07-28 20:03:32 +0800

    update #24: new solutions

--- leetcode/solution.hpp
+++ leetcode/solution.hpp
@@ -402,6 +402,32 @@ namespace s95 {
 
 
 //---------------------------------------------
+// @ID: #104
+// @Date: 2020/7/28
+// @Algorithm: Tree Algorithm
+// @Time: O(log n)
+// @Space: O(1)
+//---------------------------------------------
+namespace s104 {
+    struct TreeNode {
+        int val;
+        TreeNode* left;
+        TreeNode* right;
+        TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    };
+
+    class Solution {
+    public:
+        int maxDepth(TreeNode* root) {
+            if (!root)
+                return 0;
+            return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+        }
+    };
+}
+
+
+//---------------------------------------------
 // @ID: #122
 // @Date: 2020/7/21
 // @Algorithm: Greedy Algorithm
