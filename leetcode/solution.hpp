42ea77eaa701a451250868c3695a167f32e96656
wish142857 <wish142857@163.com>
2020-08-03 00:10:26 +0800

    update #45: new solutions

--- leetcode/solution.hpp
+++ leetcode/solution.hpp
@@ -1621,6 +1621,35 @@ namespace s413 {
 
 
 //---------------------------------------------
+// @ID: #415
+// @Date: 2020/8/3
+// @Algorithm: Simple Algorithm
+// @Time: O(max(i,j))
+// @Space: O(1)
+//---------------------------------------------
+namespace s415 {
+    class Solution {
+    public:
+        string addStrings(string num1, string num2) {
+            int i = num1.length() - 1, j = num2.length() - 1, add = 0;
+            string ans = "";
+            while (i >= 0 || j >= 0 || add != 0) {
+                int x = i >= 0 ? num1[i] - '0' : 0;
+                int y = j >= 0 ? num2[j] - '0' : 0;
+                int result = x + y + add;
+                ans.push_back('0' + result % 10);
+                add = result / 10;
+                i -= 1;
+                j -= 1;
+            }
+            reverse(ans.begin(), ans.end());
+            return ans;
+        }
+    };
+}
+
+
+//---------------------------------------------
 // @ID: #416
 // @Date: 2020/8/2
 // @Algorithm: Dynamic Programming Algorithm
