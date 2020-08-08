866cbd95b9b46967ae341d8740a786f1023afb40
wish142857 <wish142857@163.com>
2020-08-08 13:39:54 +0800

    update #57: new solutions

--- leetcode/solution.hpp
+++ leetcode/solution.hpp
@@ -719,6 +719,72 @@ namespace s98 {
 
 
 //---------------------------------------------
+// @ID: #99
+// @Date: 2020/8/8
+// @Algorithm: Tree Algorithm | Recursion Algorithm
+// @Time: O(n)
+// @Space: O(n)
+//---------------------------------------------
+namespace s99 {
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+
+    class Solution {
+    public:
+        void recoverTree(TreeNode* root) {
+            vector<int> nums;
+            inorder(root, nums);
+            pair<int, int> swapped = findTwoSwapped(nums);
+            recover(root, 2, swapped.first, swapped.second);
+        }
+
+    private:
+        void inorder(TreeNode* root, vector<int>& nums) {
+            if (root == nullptr) {
+                return;
+            }
+            inorder(root->left, nums);
+            nums.push_back(root->val);
+            inorder(root->right, nums);
+        }
+
+        pair<int, int> findTwoSwapped(vector<int>& nums) {
+            int n = nums.size();
+            int x = -1, y = -1;
+            for (int i = 0; i < n - 1; ++i) {
+                if (nums[i + 1] < nums[i]) {
+                    y = nums[i + 1];
+                    if (x == -1) {
+                        x = nums[i];
+                    }
+                    else break;
+                }
+            }
+            return { x, y };
+        }
+
+        void recover(TreeNode* r, int count, int x, int y) {
+            if (r != nullptr) {
+                if (r->val == x || r->val == y) {
+                    r->val = r->val == x ? y : x;
+                    if (--count == 0) {
+                        return;
+                    }
+                }
+                recover(r->left, count, x, y);
+                recover(r->right, count, x, y);
+            }
+        }
+    };
+}
+
+//---------------------------------------------
 // @ID: #100
 // @Date: 2020/8/7
 // @Algorithm: Tree Algorithm | Recursion Algorithm
