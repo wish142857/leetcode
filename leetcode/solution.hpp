77945a0379fa1fa56a1610f16cfc96f98b6bc042
wish142857 <wish142857@163.com>
2020-09-22 01:23:19 +0800

    update #68: new solutions

--- leetcode/solution.hpp
+++ leetcode/solution.hpp
@@ -1700,6 +1700,53 @@ namespace s213 {
 
 
 //---------------------------------------------
+// @ID: #221
+// @Date: 2020/9/22
+// @Algorithm: Dynamic Programming Algorithm
+// @Time: O(mn)
+// @Space: O(mn)
+//---------------------------------------------
+namespace s221 {
+    class Solution {
+    public:
+        int maximalSquare(vector<vector<char>>& matrix) {
+            if (matrix.size() == 0 || matrix[0].size() == 0)
+                return 0;
+
+            int m = matrix.size(), n = matrix[0].size();
+            int maxA = 0;
+
+            vector<vector<int>> dp(m, vector<int>(n));
+            for (int i = 0; i < m; i++) {
+                if (matrix[i][0] == '1') {
+                    dp[i][0] = 1;
+                    maxA = max(maxA, 1);
+                }
+            }
+            for (int j = 0; j < n; j++) {
+                if (matrix[0][j] == '1') {
+                    dp[0][j] = 1;
+                    maxA = max(maxA, 1);
+                }
+            }
+
+            for (int i = 1; i < m; i++) {
+                for (int j = 1; j < n; j++) {
+                    if (matrix[i][j] == '1') {
+                        int k = min(dp[i - 1][j], dp[i][j - 1]);
+                        dp[i][j] = (matrix[i - k][j - k] == '1' ? k + 1 : k);
+                        maxA = max(maxA, dp[i][j]);
+                    }
+                }
+            }
+
+            return maxA * maxA;
+        }
+    };
+}
+
+
+//---------------------------------------------
 // @ID: #263
 // @Date: 2020/8/2
 // @Algorithm: Simple Algorithm
@@ -1860,6 +1907,57 @@ namespace s303 {
      */
 }
 
+
+
+namespace s304 {
+        class NumMatrix {
+        public:
+            NumMatrix(vector<vector<int>>& matrix) {
+                if (matrix.size() == 0 || matrix[0].size() == 0) {
+                    dp = nullptr;
+                    return;
+                }
+                int m = matrix.size(), n = matrix[0].size();
+                dp = new vector<vector<int>>(m, vector<int>(n));
+                (*dp)[0][0] = matrix[0][0];
+                for (int i = 1; i < m; i++) {
+                    (*dp)[i][0] = (*dp)[i - 1][0] + matrix[i][0];
+                }
+                for (int j = 1; j < n; j++) {
+                    (*dp)[0][j] = (*dp)[0][j - 1] + matrix[0][j];
+                }
+
+                for (int i = 1; i < m; i++) {
+                    for (int j = 1; j < n; j++) {
+                        (*dp)[i][j] = (*dp)[i - 1][j] + (*dp)[i][j - 1] - (*dp)[i - 1][j - 1] + matrix[i][j];
+                    }
+                }
+                return;
+            }
+
+            int sumRegion(int row1, int col1, int row2, int col2) {
+                if (dp == nullptr)
+                    return 0;
+                if (row1 == 0 || col1 == 0) {
+                    if (row1 == 0 && col1 == 0)
+                        return (*dp)[row2][col2];
+                    if (row1 == 0)
+                        return (*dp)[row2][col2] - (*dp)[row2][col1 - 1];
+                    if (col1 == 0)
+                        return (*dp)[row2][col2] - (*dp)[row1 - 1][col2];
+                }
+                return (*dp)[row2][col2] - (*dp)[row1 - 1][col2] - (*dp)[row2][col1 - 1] + (*dp)[row1 - 1][col1 - 1];
+            }
+        private:
+            vector<vector<int>> *dp;
+        };
+
+    /**
+     * Your NumMatrix object will be instantiated and called as such:
+     * NumMatrix* obj = new NumMatrix(matrix);
+     * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+     */
+}
 
 //---------------------------------------------
 // @ID: #312
