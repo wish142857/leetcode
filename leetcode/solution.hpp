e847a779bd64a89f1609ac3b09b57d973fa7a0ea
wish142857 <wish142857@163.com>
2020-09-23 13:23:39 +0800

    update #73: new solutions

--- leetcode/solution.hpp
+++ leetcode/solution.hpp
@@ -3267,6 +3267,37 @@ namespace s538 {
 
 
 //---------------------------------------------
+// @ID: #617
+// @Date: 2020/9/23
+// @Algorithm: Tree Algorithm
+// @Time: O(n)
+// @Space: O(n)
+//---------------------------------------------
+namespace s617 {
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    };
+
+    class Solution {
+    public:
+        TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+            if (t1 == nullptr)
+                return t2;
+            if (t2 == nullptr)
+                return t1;
+            t1->val += t2->val;
+            t1->left = mergeTrees(t1->left, t2->left);
+            t1->right = mergeTrees(t1->right, t2->right);
+            return t1;
+        }
+    };
+}
+
+
+//---------------------------------------------
 // @ID: #687
 // @Date: 2020/7/31
 // @Algorithm: Recursion Algorithm
