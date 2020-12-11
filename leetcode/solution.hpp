a9796f1aedcc8ffb129f3e50f62ad0430b0565b6
wish142857 <wish142857@163.com>
2020-12-11 11:46:13 +0800

    update #117

--- leetcode/solution.hpp
+++ leetcode/solution.hpp
@@ -4858,6 +4858,43 @@ namespace s637 {
 
 
 //---------------------------------------------
+// @ID: #649
+// @Date: 2020/12/11
+// @Algorithm: Greedy Algorithm
+// @Time: O(n)
+// @Space: O(n)
+//---------------------------------------------
+namespace s649 {
+    class Solution {
+    public:
+        string predictPartyVictory(string senate) {
+            int n = senate.size();
+            queue<int> radiant, dire;
+            for (int i = 0; i < n; ++i) {
+                if (senate[i] == 'R') {
+                    radiant.push(i);
+                }
+                else {
+                    dire.push(i);
+                }
+            }
+            while (!radiant.empty() && !dire.empty()) {
+                if (radiant.front() < dire.front()) {
+                    radiant.push(radiant.front() + n);
+                }
+                else {
+                    dire.push(dire.front() + n);
+                }
+                radiant.pop();
+                dire.pop();
+            }
+            return !radiant.empty() ? "Radiant" : "Dire";
+        }
+    };
+}
+
+
+//---------------------------------------------
 // @ID: #687
 // @Date: 2020/7/31
 // @Algorithm: Recursion Algorithm
