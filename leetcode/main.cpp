f90538937439969f6e9c5f3113ff46dcbe2ff3c2
wish142857 <wish142857@163.com>
2021-03-16 16:31:45 +0800

    update #146

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -10,6 +10,7 @@
 
 
 
+
 int main() {
 	std::cout << "Main::Compiled successfully" << std::endl;
 	return 0;
--- leetcode/solution.hpp
+++ leetcode/solution.hpp
@@ -1166,6 +1166,39 @@ namespace s57 {
 
 
 //---------------------------------------------
+// @ID: #59
+// @Date: 2021/3/16
+// @Algorithm: Simple Algorithm
+// @Time: O(n^2)
+// @Space: O(n^2)
+//---------------------------------------------
+namespace s59 {
+    class Solution {
+    public:
+        vector<vector<int>> generateMatrix(int n) {
+            int maxNum = n * n;
+            int curNum = 1;
+            vector<vector<int>> matrix(n, vector<int>(n));
+            int row = 0, column = 0;
+            vector<vector<int>> directions = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+            int directionIndex = 0;
+            while (curNum <= maxNum) {
+                matrix[row][column] = curNum;
+                curNum++;
+                int nextRow = row + directions[directionIndex][0], nextColumn = column + directions[directionIndex][1];
+                if (nextRow < 0 || nextRow >= n || nextColumn < 0 || nextColumn >= n || matrix[nextRow][nextColumn] != 0) {
+                    directionIndex = (directionIndex + 1) % 4;
+                }
+                row = row + directions[directionIndex][0];
+                column = column + directions[directionIndex][1];
+            }
+            return matrix;
+        }
+    };
+}
+
+
+//---------------------------------------------
 // @ID: #61
 // @Date: 2021/3/1
 // @Algorithm: Linked List Algorithm
