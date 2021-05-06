d101c9c3edeacab36cd9613b0fe1aa7dc07c51e7
wish142857 <wish142857@163.com>
2021-05-06 14:10:52 +0800

    update #170

--- leetcode/solution.hpp
+++ leetcode/solution.hpp
@@ -11007,6 +11007,28 @@ namespace s1711 {
 
 
 //---------------------------------------------
+// @ID: #1720
+// @Date: 2021/5/6
+// @Algorithm: Simple Algorithm
+// @Time: O(n)
+// @Space: O(n)
+//---------------------------------------------
+namespace s1720 {
+    class Solution {
+    public:
+        vector<int> decode(vector<int>& encoded, int first) {
+            int n = encoded.size();
+            vector<int> ans;
+            ans.push_back(first);
+            for (int i = 0; i < n; i++)
+                ans.push_back(ans[i] ^ encoded[i]);
+            return ans;
+        }
+    };
+}
+
+
+//---------------------------------------------
 // @ID: #1721
 // @Date: 2021/3/9
 // @Algorithm: Linked List Algorithm
